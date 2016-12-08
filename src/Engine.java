14b1f8974c0a391c8b5a151a00720b5c75c655c1
James <James@100.71.59.5>
2016-12-08 02:07:57 +0000

    Big Changes

--- src/Engine.java
+++ src/Engine.java
@@ -3,124 +3,89 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Engine {
+	
+	final String TEST_URL = "src/cwk_test.csv";
+	final int POP = 10;
+	
 	public Engine(){
-		String url = "C:/Users/James/Desktop/Final Year/Computational Intelligence/Coursework/workspace/cwk_test.csv";
-		//ArrayList<HashMap<String,Double>> pop = populate(url);
+
+		//Initialisation:
+			//Make some random functions
+		ArrayList<ArrayList<ExpressionPart>> randomFunctions = initialFunctionList(POP);
 		
+		
+		//EVAL collection
+		for(int i = 0; i<randomFunctions.size(); i++){
+			// for every function
+			// combine operators from function and data
+			// apply to every row and create a score
+			// store function and score somewhere
+			
+		}
+		
+		//SELECT parents
+		//RECOMBINE parents
+		//MUTATE offspring
+		//EVAL offspring
+		
+	}
+	
+	public ArrayList<ArrayList<Operand>> popDataTable(){
+		ArrayList<ArrayList<Operand>> dataTable = new ArrayList<ArrayList<Operand>>();
 		Scanner scanner;
-		ArrayList<Double> expected = new ArrayList<Double>();
-		ArrayList<ArrayList<ExpressionPart>> trees = new ArrayList<ArrayList<ExpressionPart>>();
-		
 		try {
-			scanner = new Scanner(new File(url));
+			scanner = new Scanner(new File(TEST_URL));
 			scanner.useDelimiter(",");
 			
-			int lineNumber = 1;
-	        while(scanner.hasNextLine()){
+			while(scanner.hasNextLine()){
 	        	String line = scanner.nextLine();
 	        	String[] lineSplit = line.split(",");
-	        	expected.add(Double.parseDouble(lineSplit[0]));
-	        	ArrayList<ExpressionPart> treeData = new ArrayList<ExpressionPart>();
+	        	ArrayList<Operand> treeData = new ArrayList<Operand>();
 	        	
 	        	for(int i = 1; i<lineSplit.length; i++){
-	        		
 	        		treeData.add(new Operand(Double.parseDouble(lineSplit[i])));
-	        		
-	        		if(i != lineSplit.length){
-	        			treeData.add(new Operator());
 	        		}
-	        		
-	        		//fake treeData
-	        		ArrayList<ExpressionPart> fake = new ArrayList<ExpressionPart>();
-	        		Operand o1 = new Operand(1.0);
-	        		Operator o2 = new Operator('+');
-	        		Operand o3 = new Operand(2.0);
-	        		Operator o4 = new Operator('+');
-	        		Operand o5 = new Operand(3.0);
-	        		fake.add(o1);
-	        		fake.add(o2);
-	        		fake.add(o3);
-	        		fake.add(o4);
-	        		fake.add(o5);
-	        		
-	        		//create tree
-	        		//add treeData to a Tree
-	        		//tree.build(treeData);
-	        		ExpressionTree<ExpressionPart> tree = new ExpressionTree<ExpressionPart>();
-	        		tree.build(fake);
-	        		System.out.println(tree.evaluate());
-	        		//store in trees
-	        		trees.add(fake);
-	        		lineNumber++;	
-	        	}	
-	        }
-	        scanner.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	        	dataTable.add(treeData);
 		}
 		
 		
-		for(int i = 0; i<trees.get(0).size(); i++){
-			//System.out.print(trees.get(0).get(i)+", ");
+		} catch(FileNotFoundException e){
+		
+		}		
+		
+		return dataTable;
+}
+
+	
+	public ArrayList<ArrayList<ExpressionPart>> initialFunctionList(int popSize){
+		ArrayList<ArrayList<ExpressionPart>> list = new ArrayList<ArrayList<ExpressionPart>>();
+		
+		for(int index = 0; index<popSize; index++){
+			ArrayList<ExpressionPart> subList = new ArrayList<ExpressionPart>();
+			for(int i = 0; i<12; i++){
+				Operator op = new Operator();
+				op.changeRandomly();
+				subList.add(op);
+			}
+			list.add(subList);
 		}
-		
-		
-		
-		//for(int i = 0; i<pop.size();i++){
-		//	System.out.println(pop.get(i).toString());
-		//	System.out.println("**********************");
-		//}
+		return list;
 	}
 	
-	public ArrayList<HashMap<String,Double>> populate(String url){
-		
-		ArrayList<HashMap<String,Double>> population = new ArrayList<HashMap<String,Double>>();																	
-		Scanner scanner;
-		
-			try {
-				scanner = new Scanner(new File(url));
-				scanner.useDelimiter(",");
-		        while(scanner.hasNextLine()){
-		        	String line = scanner.nextLine();
-		        	String[] lineSplit = line.split(",");  
-		        	ArrayList<String> lineSplitNew = new ArrayList<String>();
-		        	HashMap<String,Double> rows = new HashMap<String,Double>();
-		        	rows.put("expected", Double.parseDouble(lineSplit[0]));
-		        	for(int i = 1; i<lineSplit.length; i++){
-		        		lineSplitNew.add(lineSplit[i]);
-		        	}
-		        	
-		        	for(int i = 0; i<lineSplitNew.size(); i++){
-		        		rows.put(toAlphabetic(i), Double.parseDouble(lineSplitNew.get(i)));
-		        	}        
-		        	population.add(rows);
-		        }
-		        scanner.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	public void listToString(ArrayList<ArrayList<ExpressionPart>> functions){
+		for(int i = 0; i<functions.size();i++){
+			System.out.println("Function " + i + ":");
+			for(int j = 0; j<functions.get(i).size(); j++){
+				System.out.print(functions.get(i).get(j).toString());
 			}
-			 
-		        return population;
-		    }
-	
-	public static String toAlphabetic(int i) {
-	    if( i<0 ) {
-	        return "-"+toAlphabetic(-i-1);
-	    }
-
-	    int quot = i/26;
-	    int rem = i%26;
-	    char letter = (char)((int)'A' + rem);
-	    if( quot == 0 ) {
-	        return ""+letter;
-	    } else {
-	        return toAlphabetic(quot-1) + letter;
-	    }
+		}
 	}
 }
+
+	
+
 
 
---
+++ src/ExpTree.java
@@ -0,0 +1,212 @@
+import java.util.ArrayList;
+
+public class ExpTree<T> {
+	 /** class TreeNode **/
+    class TreeNode
+    {    
+        ExpressionPart data;
+        TreeNode left, right;
+ 
+        /** constructor **/
+        public TreeNode(ExpressionPart expressionPart)
+        {
+            this.data = expressionPart;
+            this.left = null;
+            this.right = null;
+        }
+    } 
+ 
+    /** class StackNode **/
+    class StackNode
+    {
+        TreeNode treeNode;
+        StackNode next;
+ 
+        /** constructor **/
+        public StackNode(TreeNode treeNode)
+        {
+            this.treeNode = treeNode;
+            next = null;
+        }
+    }
+ 
+    private StackNode top;
+ 
+    /** constructor **/
+    public ExpTree()
+    {
+        top = null;
+    }
+ 
+    /** function to clear tree **/
+    public void clear()
+    {
+        top = null;
+    }
+ 
+    /** function to push a node **/
+    private void push(TreeNode ptr)
+    {
+        if (top == null)
+            top = new StackNode(ptr);
+        else
+        {
+            StackNode nptr = new StackNode(ptr);
+            nptr.next = top;
+            top = nptr;
+        }
+    }
+ 
+    /** function to pop a node **/
+    private TreeNode pop()
+    {
+        if (top == null)
+            throw new RuntimeException("Underflow");
+        else
+        {
+            TreeNode ptr = top.treeNode;
+            top = top.next;
+            return ptr;
+        }
+    }
+ 
+    /** function to get top node **/
+    private TreeNode peek()
+    {
+        return top.treeNode;
+    }
+ 
+    /** function to insert character **/
+    private void insert(ExpressionPart expressionPart)
+    {
+        try
+        {
+            if (expressionPart instanceof Operand)
+            {
+                TreeNode nptr = new TreeNode(expressionPart);
+                push(nptr);
+            }
+            else if (expressionPart  instanceof Operator)
+            {
+                TreeNode nptr = new TreeNode(expressionPart);
+                nptr.left = pop();
+                nptr.right = pop();
+                push(nptr);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Invalid Expression");
+        }
+    }
+ 
+    /** function to check if digit **/
+    private boolean isDigit(char ch)
+    {
+        return ch >= '0' && ch <= '9';
+    }
+ 
+    /** function to check if operator **/
+    private boolean isOperator(char ch)
+    {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    }
+ 
+    /** function to convert character to digit **/
+    private int toDigit(char ch)
+    {
+        return ch - '0';
+    }
+ 
+    /** function to build tree from input */
+    public void buildTree(ArrayList<ExpressionPart> exp)
+    {
+        for (int i = exp.size() - 1; i >= 0; i--){
+            insert(exp.get(i));
+        }
+        
+    }
+ 
+    /** function to evaluate tree */
+    public double evaluate()
+    {
+        return evaluate(peek());
+    }
+ 
+    /** function to evaluate tree */
+    public double evaluate(TreeNode ptr)
+    {
+        if (ptr.left == null && ptr.right == null)
+            return Double.parseDouble(ptr.data.toString());
+        else
+        {
+            double result = 0.0;
+            double left = evaluate(ptr.left);
+            double right = evaluate(ptr.right);
+            char operator = ptr.data.toString().charAt(0);
+ 
+            switch (operator)
+            {
+            case '+' : result = left + right; break;
+            case '-' : result = left - right; break;
+            case '*' : result = left * right; break;
+            case '/' : result = left / right; break;
+            default  : result = left + right; break;
+            }
+            return result;
+        }
+    }
+ 
+    /** function to get postfix expression */
+    public void postfix()
+    {
+        postOrder(peek());
+    }
+ 
+    /** post order traversal */
+    private void postOrder(TreeNode ptr)
+    {
+        if (ptr != null)
+        {
+            postOrder(ptr.left);            
+            postOrder(ptr.right);
+            System.out.print(ptr.data);            
+        }    
+    }
+ 
+    /** function to get infix expression */
+    public void infix()
+    {
+        inOrder(peek());
+    }
+ 
+    /** in order traversal */
+    private void inOrder(TreeNode ptr)
+    {
+        if (ptr != null)
+        {
+            inOrder(ptr.left);
+            System.out.print(ptr.data);
+            inOrder(ptr.right);            
+        }    
+    }
+ 
+    /** function to get prefix expression */
+    public void prefix()
+    {
+        preOrder(peek());
+    }
+ 
+    /** pre order traversal */
+    private void preOrder(TreeNode ptr)
+    {
+        if (ptr != null)
+        {
+            System.out.print(ptr.data);
+            preOrder(ptr.left);
+            preOrder(ptr.right);            
+        }    
+    }
+}
+
+
